d044644d0ad59201bae5c1114e17cf2c55133dc9
javavinoth <vinoth.sakthivel95@gmail.com>
2019-12-21 19:22:07 +0530

    Updates

---
+++ precache-manifest.bcfb6290b2693de0e78848da45b3a783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae70e83cd22251b37402e4cf66bdcd9",
+    "url": "/react-helloworld/index.html"
+  },
+  {
+    "revision": "1caf27c0780046244266",
+    "url": "/react-helloworld/static/css/main.755de8e4.chunk.css"
+  },
+  {
+    "revision": "12ed7e1fae371b4cb936",
+    "url": "/react-helloworld/static/js/2.e494a0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-helloworld/static/js/2.e494a0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1caf27c0780046244266",
+    "url": "/react-helloworld/static/js/main.30162831.chunk.js"
+  },
+  {
+    "revision": "4da8e08b55117c268aa3",
+    "url": "/react-helloworld/static/js/runtime-main.f7211d02.js"
+  }
+]);
